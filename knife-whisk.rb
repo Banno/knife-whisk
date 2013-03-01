5ab95fb4f1d1f988f0a29674c0ece444a0d04d26
Nic Grayson <nic.grayson@banno.com>
2013-03-01 09:18:26 -0600

    swapped how the get_config method is called

--- knife-whisk.rb
+++ knife-whisk.rb
@@ -1,57 +1,68 @@
 require 'chef/knife'
 require 'yaml'
 
-module ConfigLoader
-  def get_config
-    # if File.exists?('/Users/ngrayson/banno/knife-whisk/whisk.yml')
-    if File.exists?(::Chef::Knife::chef_config_dir+"/whisk.yml")
-      YAML.load_file(::Chef::Knife::chef_config_dir+"/whisk.yml")
-    else
-      puts "Required whisk.yml does not exist"
-      exit 1
-    end
-  end
-end
-
-module KnifeWhisk
-  class Whisk < Chef::Knife
-    banner "knife whisk"
-    def run
-      ui.fatal "Did you mean \"knife whisk list\" instead?"
-      show_usage
-      exit 1
-    end
-  end
-  
-  class WhiskList < Chef::Knife
-    include ConfigLoader
-    banner "knife whisk list"
-    def run
-      full_hash = get_config
-      server_list = full_hash["servers"]
-      server_list.each do |server|
-        puts server.first
+#module KnifeWhisk
+class Chef
+  class Knife
+    module ConfigLoader
+      def self.included(includer)
+        includer.class_eval do
+          option :custom_path,
+            :short => '-C',
+            :log => '--config_path',
+            :boolean => true,
+            :description => "Specify path to your whisk.yml file"
+        end
       end
+            
+      def get_config
+        if File.exists?(::Chef::Knife::chef_config_dir+"/whisk.yml")
+          YAML.load_file(::Chef::Knife::chef_config_dir+"/whisk.yml")
+        else
+          puts "Required whisk.yml does not exist"
+          exit 1
+        end
+      end    
     end
   end
-  
-  class WhiskShow < Chef::Knife
-    include ConfigLoader
-    banner "knife whisk show TEMPLATENAME"
-    def run
-      unless name_args.size == 1
-        ui.fatal "You must specify a template name"
+      
+    class Whisk < Chef::Knife
+      banner "knife whisk"
+      def run
+        ui.fatal "Did you mean \"knife whisk list\" instead?"
         show_usage
         exit 1
       end
-      config_file = get_config
-      puts config_file["#{name_args.first}"]
     end
-  end
+    
+    class WhiskList < Chef::Knife
+      include Knife::ConfigLoader
+      banner "knife whisk list"
+      def run
+        server_list = get_config["servers"]
+        server_list.each do |server|
+          puts server.first
+        end
+      end
+    end
+    
+    class WhiskShow < Chef::Knife
+      include Knife::ConfigLoader
+      banner "knife whisk show TEMPLATENAME"
+      def run
+        unless name_args.size == 1
+          ui.fatal "You must specify a template name"
+          show_usage
+          exit 1
+        end
+        server_list = get_config["servers"]
+        puts server_list["#{name_args.first}"].to_yaml
+      end
+    end
 
-  class WhiskGenerate < Chef::Knife
-    banner "knife whisk generate NODENAME"
-  end
+    class WhiskGenerate < Chef::Knife
+      banner "knife whisk generate NODENAME"
+    end
 
   class WhiskHistory < Chef::Knife
     banner "knife whisk history"
